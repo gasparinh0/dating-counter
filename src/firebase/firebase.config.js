f0ff06cda342af82ec06113d4f66bf71b7ae78a2
Lucas Gaspari <lcsgaspari@gmail.com>
2024-12-17 14:33:57 -0300

    refactor(firebase): adding env to keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCU1G7B_9N0O2jvICuc083hP49yXsj_QzA",
-  authDomain: "dating-counter.firebaseapp.com",
-  databaseURL: "https://dating-counter-default-rtdb.firebaseio.com",
+  apiKey: process.env.FIREBASE_APY_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: "dating-counter",
   storageBucket: "dating-counter.firebasestorage.app",
-  messagingSenderId: "665099090633",
-  appId: "1:665099090633:web:7570d551c9c6175d162b18",
-  measurementId: "G-P6N064TC09"
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
